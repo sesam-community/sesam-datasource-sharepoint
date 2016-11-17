3e96518960b7290a39d39a4a938bc2f91b204b2f
Axel Borge <axel.borge@bouvet.no>
2016-11-17 09:19:38 +0100

    Add config from dataset

--- service/datasource-service.py
+++ service/datasource-service.py
@@ -1,11 +1,16 @@
+from functools import wraps
 from flask import Flask, request, Response, abort
 from datetime import datetime, timedelta
 import json
 import requests
+import operator
+import os
 from requests_ntlm import HttpNtlmAuth
 
 
 app = Flask(__name__)
+config = {}
+config_since = 0
 
 
 class DataAccess:
@@ -13,13 +18,17 @@ class DataAccess:
         self._entities = {"users": [], "groups": [], "documents": []}
 
     def get_entities(self, since, datatype, user, password):
-        siteurl = "https://kunder.bouvet.no/Sesam"
         if not datatype in self._entities:
             abort(404)
-        if since is None:
-            return self.get_entitiesdata(siteurl, datatype, since, user, password)
-        else:
-            return [entity for entity in self.get_entitiesdata(siteurl, datatype, since) if entity["_updated"] > since]
+        result = []
+        for s in config:
+            if "site-url" in s:
+                siteurl = s["site-url"]
+                if since is None:
+                    result.extend( self.get_entitiesdata(siteurl, datatype, since, user, password))
+                else:
+                    result.extend( [entity for entity in self.get_entitiesdata(siteurl, datatype, since) if entity["_updated"] > since])
+        return result
 
     def get_entitiesdata(self, siteurl, datatype, since, user, password):
         if datatype in self._entities:
@@ -73,11 +82,52 @@ class DataAccess:
 
 data_access_layer = DataAccess()
 
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
-@app.route('/<user>/<password>/<datatype>')
-def get_entities(datatype, user, password):
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth:
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+
+def read_config(config_url):
+    global config_since
+    global config
+    r = requests.get(config_url + "?since=%s&history=false" % (str(config_since-1)))
+    change = json.loads(r.text)
+    for changed_item in change:
+        changed_item_id = changed_item["_id"]
+        if changed_item["_deleted"]:
+            if changed_item_id in config:
+                del config[changed_item_id]
+        else:
+            config[changed_item_id] = changed_item
+        changed_item_updated = changed_item["_updated"]
+        if config_since is None or changed_item_updated > config_since:
+            config_since = changed_item_updated
+
+@app.route('/<datatype>')
+@requires_auth
+def get_entities(datatype):
     since = request.args.get('since')
-    entities = data_access_layer.get_entities(since, datatype, user, password)
+    conf = None
+    if 'CONFIG_DATASET' in os.environ:
+        conf = os.environ['CONFIG_DATASET']
+    if not conf:
+        conf = request.args.get('config-dataset')
+    if conf:
+        read_config(conf)
+    auth = request.authorization
+    entities = data_access_layer.get_entities(since, datatype, auth.username, auth.password)
     return Response(json.dumps(entities), mimetype='application/json')
 
 if __name__ == '__main__':
